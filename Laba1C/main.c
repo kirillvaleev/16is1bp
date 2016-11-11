a34145e3ef6060c49f75bc550da926fc3d91e11e
kirillvaleev <kirill.yozhikov@gmail.com>
2016-11-11 17:01:33 +0300

    Laba1 C

--- Laba1C/main.c
+++ Laba1C/main.c
@@ -3,16 +3,18 @@
 
 int main()
 {
-    int x,a;
-    float G, F, Y;
+    double x, a, G, F, Y;
+    char Function;
 
     printf("Введите Х:");
-    scanf("%i", &x);
+    scanf("%lf", &x);
     printf("Введите A:");
-    scanf("%i-", &a);
+    scanf("%lf", &a);
+    printf("Введите название функции(G, F, Y):\n");
 
-    G = 3*(-3*a*a+2*a*x+21*x*x)/(35*a*a+37*a*x+6*x*x);
-    F = (float) (1 / cos(3 * a * a + 5 * a * x + 2 * x * x));
+
+    G = (3*(-3*a*a+2*a*x+21*x*x)/(35*a*a+37*a*x+6*x*x));
+    F = (1 / cos(3 * a * a + 5 * a * x + 2 * x * x));
     Y = (-12*a*a-4*a*x+x*x+1);
 
     printf("G = %.4f\nF = %.4f\nY = %.4f", G, F, Y);
