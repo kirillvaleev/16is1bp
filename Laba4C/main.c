53850421e3be2be444687b3ec0ac67e6baba7a2a
kirillvaleev <kirill.yozhikov@gmail.com>
2016-12-09 02:18:28 +0300

    Laba4 C

--- Laba4C/main.c
+++ Laba4C/main.c
@@ -5,20 +5,18 @@
 int main()
 {
     int index_mas,si_mas;
-    double sx, ex, sa, ea, x, a, G, F, Y, step, min_mas, max_mas;
+    double sx, ex, sa, ea, x, a, G, F, Y, step, start_result, end_result, min_mas, max_mas;
     char Function, off;
     Start:
     printf("Введите начальное значение Х:");
     scanf("%lf", &sx);
     printf("Введите конечное значение Х:");
     scanf("%lf", &ex);
-    printf("Введите начальное значение A:");
-    scanf("%lf", &sa);
-    printf("Введите конечное значение A:");
-    scanf("%lf", &ea);
+    printf("Введите значение A:");
+    scanf("%lf", &a);
     printf("Введите шаг:");
     scanf("%lf", &step);
-    index_mas = (int) ceil(fabs(sx) * fabs(ex) * fabs(sa) * fabs(ea) / step + 10);
+    index_mas = (int) ceil(fabs(sx) * fabs(ex) / step + 10);
     double mas[index_mas];
     si_mas=0;
     Func:
@@ -28,7 +26,6 @@ int main()
     {
         case 'G':
             for (x = sx; x <= ex; x += step)
-                for (a = sa; a <= ea; a += step)
                 {
                     if (fabs(35 * a * a + 37 * a * x + 6 * x * x) >= 0.0001)
                     {
@@ -42,7 +39,6 @@ int main()
             break;
         case 'F':
             for (x = sx; x <= ex; x += step)
-                for (a = sa; a <= ea; a += step)
                 {
                     if (fabs(cos(3 * a * a + 5 * a * x + 2 * x * x)) >= 0.0001)
                     {
@@ -56,7 +52,6 @@ int main()
             break;
         case 'Y':
             for (x = sx; x <= ex; x += step)
-                for (a = sa; a <= ea; a += step)
                 {
                     if (fabs(-12 * a * a - 4 * a * x + x * x + 1) <= 1)
                     {
@@ -79,13 +74,12 @@ int main()
     printf("║    X    ║    A    ║    %c    ║\n", Function);
     printf("╠═════════╬═════════╬═════════╣\n");
     for (x = sx; x <= ex; x += step)
-        for (a = sa; a <= ea; a += step)
         {
             printf("║%9.3lf║%9.3lf║%9.3lf║\n", x, a, mas[si_mas]);
             si_mas+=1;
             if (mas[si_mas] > max_mas)
                 max_mas = mas[si_mas];
-            if (mas[si_mas] < min_mas)
+            if ((mas[si_mas] < min_mas) && (fabs(mas[si_mas])>=0.001))
                 min_mas = mas[si_mas];
         }
     printf("╚═════════╩═════════╩═════════╝\n");
